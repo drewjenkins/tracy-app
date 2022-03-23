5a2151aa06121e6c90817b18ac4c1f537d6bc84f
Andrew jenkins <ajenkins@nativ.co>
2022-03-23 10:08:38 -0400

    Jordan

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "./index.css";
+
+const helloWorld = document.createElement("div");
+helloWorld.innerText = "Hello World";
+helloWorld.className = "hello";
+document.body.appendChild(helloWorld);
